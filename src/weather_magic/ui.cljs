7c503cd0bbffe6bccfc46c9e7a384c48ceb75e42
Maja Ilestrand <maja@c-214-179.eduroam.liu.se>
2016-10-11 15:01:53 +0200

    commit för att kolla på PR

--- src/weather_magic/ui.cljs
+++ src/weather_magic/ui.cljs
@@ -27,20 +27,26 @@
 ;; TEST
 (def blur (atom 0))
 
+(defn blur-obj []
+  [:div {:id "blur"}])
+
+
 (defn counting-component []
   [:div
-   "The atom " [:code "blur"] " has value: "
+  [:code " blur"] " has value: "
    @blur ". "
    [:input {:type "button" :value "Read more!"
-            :on-click #(swap! blur inc)}]])
+           :on-click #(swap! blur inc)}]])
 ;;
 
 (defn map-ui
   "The UI displayed while the user interacts with the map."
   []
   [:span
-   [:p "Hello everybody, allihopa!"]
-   [time-slider]])
+  [:p "Hello everybody, allihopa!"]
+  [time-slider]
+  [counting-component]]
+  )
 
 (defn mount-ui!
   "Place the user interface into the DOM."
