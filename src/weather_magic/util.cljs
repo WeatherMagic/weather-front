f52f686fd5aa5e3ba82de74cb34ab823ffcfbe51
Alexander Poole <alex.o.poole@gmail.com>
2016-11-28 14:59:39 +0100

    Added transform between latlon and uv

--- src/weather_magic/util.cljs
+++ src/weather_magic/util.cljs
@@ -1,7 +1,8 @@
 (ns weather-magic.util
   (:require
    [thi.ng.geom.gl.buffers :as buf]
-   [thi.ng.geom.gl.webgl.constants :as glc]))
+   [thi.ng.geom.gl.webgl.constants :as glc]
+   [thi.ng.geom.vector  :refer vec2]))
 
 (defn transparent-println
   "Print something and return that something."
@@ -24,3 +25,10 @@
   ;; name), possibly followed by a '.' and some characters not '/' (the
   ;; filename extension), always ended with a line ending.
   (second (re-find #"([^/^\.]+)\.?[^/]*$" path)))
+
+(defn latlon-to-uv
+  [lat lon]
+  (let
+      [u (+ (/ lon 360) 1/2)
+       v (+(/ lat 180) 1/2)])
+  (vec2 u v))
