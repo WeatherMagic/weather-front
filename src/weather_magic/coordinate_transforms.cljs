1b660a55af2c55f2293aaae7e31312b4cb246d53
majil334 <majil334@student.liu.se>
2016-11-28 16:27:51 +0100

    flyttade till util och returnerar nu värden

--- src/weather_magic/coordinate_transforms.cljs
+++ src/weather_magic/coordinate_transforms.cljs
@@ -2,29 +2,3 @@
   (:require
    [weather-magic.state :as state]
    [thi.ng.math.core :as m :refer [PI HALF_PI TWO_PI]]))
-
-(defn lat-lon-to-cart
-  "Converting latitude and longitude to model cordinates"
-  [lat lon]
-  (let [rlat (m/radians lat)
-        rlon (m/radians lon)]
-    {:x (* (Math/sin rlon) (Math/cos rlat))
-     :y (Math/sin rlat)
-     :z (* (Math/cos rlon) (Math/cos rlat))}))
-
-(defn cart-to-lat-lon
-  "Converting latitude and longitude to model cordinates"
-  [x y z]
-  (let [eps 0.001]
-    (if (> (Math/abs z) eps)
-      (swap! state/geografic-point assoc :lon (* (/ 180 PI) (Math/atan2 x z)))
-      (if (> (Math/abs y) (- 1 eps))
-        (swap! state/geografic-point assoc :lon 0)
-        (if (pos? x)
-          (swap! state/geografic-point assoc :lon 90)
-          (swap! state/geografic-point assoc :lon -90))))
-    (if (> y (- 1 eps))
-      (swap! state/geografic-point assoc :lat 90)
-      (if (< y (+ -1 eps))
-        (swap! state/geografic-point assoc :lat -90)
-        (swap! state/geografic-point assoc :lat (* (/ 180 PI) (Math/asin y)))))))
