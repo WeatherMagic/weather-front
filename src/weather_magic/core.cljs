82c8c1fadb473fc60e16eecc7155d7aa0ae80655
majil334 <majil334@student.liu.se>
2016-12-12 15:39:51 +0100

    watchers for skybox and zoom

--- src/weather_magic/core.cljs
+++ src/weather_magic/core.cljs
@@ -118,8 +118,10 @@
 (watchers/mount-rotation-data-reload-watch state/earth-orientation
                                            #(cache/trigger-data-load! true))
 
-(watchers/on-change-date-watch state/date-atom
-                               #(cache/trigger-data-load! true))
+(watchers/on-change-date-watch state/date-atom cache/trigger-data-load!)
+
+(watchers/mount-zoom-data-reload-watch state/camera-left
+                                       #(cache/trigger-data-load! true))
 
 ;; This is a hook for figwheel, add stuff you want run after you save your source.
 (defn on-js-reload [])
--- src/weather_magic/watchers.cljs
+++ src/weather_magic/watchers.cljs
@@ -30,3 +30,25 @@
                  (when-not (= (:value (:month (:right @date-atom-last-request))) (:value (:month (:right new-state))))
                    (vreset! date-atom-last-request new-state)
                    (reload-fn :right true))))))
+
+(defn mount-zoom-data-reload-watch
+  [camera reload-fn]
+  (let [cam-last-request (volatile! @camera)]
+    (add-watch camera :trigger-data-load-zoom
+               (fn [key reference old-state new-state]
+                 (let [camera-z-pos-old (aget (.-buf (:eye @cam-last-request)) 2)
+                       zoom-level-old (/ (- camera-z-pos-old 1.1) 3.0)
+                       camera-z-pos-new (aget (.-buf (:eye new-state)) 2)
+                       zoom-level-new (/ (- camera-z-pos-new 1.1) 3.0)]
+                   (when (and (> zoom-level-old 0.16) (< zoom-level-new 0.16))
+                     (vreset! cam-last-request new-state)
+                     (reload-fn))
+                   (when (and (> zoom-level-old 0.06) (< zoom-level-new 0.06))
+                     (vreset! cam-last-request new-state)
+                     (reload-fn))
+                   (when (and (< zoom-level-old 0.16) (> zoom-level-new 0.16))
+                     (vreset! cam-last-request new-state)
+                     (reload-fn))
+                   (when (and (< zoom-level-old 0.06) (> zoom-level-new 0.06))
+                     (vreset! cam-last-request new-state)
+                     (reload-fn)))))))
